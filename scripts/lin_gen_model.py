4168f1e65f47fa206b1b27bf49253302e0452e84
Sam Kelemen <86901068+samkelemen@users.noreply.github.com>
2025-03-04 18:57:56 -0600

    Update lin_gen_model.py

--- scripts/lin_gen_model.py
+++ scripts/lin_gen_model.py
@@ -7,11 +7,8 @@ import math
 import numpy as np
 from numpy.typing import NDArray
 from sklearn.linear_model import Lasso
-from statsmodels.regression.linear_model import OLS
-import torch
-import pyro
-import pyro.distributions as dist
-from pyro.infer import MCMC, NUTS
+from sklearn.metrics import r2_score
+from scipy import stats
 
 
 def algebraic_linear_regression(X: NDArray[np.float64], y: NDArray[np.float64]) -> NDArray[np.float64]:
@@ -33,6 +30,7 @@ def calc_alpha_grid(X: NDArray[np.float64], y: NDArray[np.float64], alpha_min_ma
             inner_products.append(np.dot(row, y))
         alpha_max = max(inner_products) / N
         alpha_min = 0.0001 * alpha_max
+        alpha_max = 5 * alpha_min # This was experientially found to be a better upper bound, as the selected alpha never came close to 5 * min
     else:
         alpha_min = alpha_min_max[0]
         alpha_max = alpha_min_max[1]
@@ -53,102 +51,51 @@ def lasso_regression(X: NDArray[np.float64], y: NDArray[np.float64], alpha: floa
 
     return beta, lasso_model
 
-def bic_selection(X: NDArray[np.float64], y: NDArray[np.float64], train_func: Callable, alpha_vals: list[float], priors: NDArray[np.float64]=None) -> tuple[float, NDArray[np.float64] | Any]:
+def aic_selection(X: NDArray[np.float64], y: NDArray[np.float64], train_func: Callable, alpha_vals: list[float], log_path: str=None) -> tuple[float, NDArray[np.float64] | Any]:
     """
     Fits a Lasso regression model for each alpha value and selects the one with the lowest BIC.
     """
+    # Small constant to avoid log(0)
+    eps = 1e-10
+
     # Initialize the best BIC and corresponding alpha value
-    best_bic = np.inf
+    best_aic = np.inf
     best_alpha = None
     best_model = None
 
     # Loop over the alpha values
     for alpha in alpha_vals:
-        beta, curr_model = train_func(X, y, priors, alpha) if priors else train_func(X, y, alpha)
+        beta, curr_model = train_func(X, y, alpha)
 
-        # Create a new sc matrix with only the selected features (non-zero coefficients)
-        selected_features = np.where(beta != 0)[0]
-        X_selected = X[:, selected_features]
+        # Calculate AIC
+        predy = X @ beta
+        residuals = y - predy
+        RSS = np.sum(residuals**2)
 
-        # Fit an OLS model using statsmodels to get the BIC
-        num_selected_features = np.shape(X_selected)[0]
+        k = np.count_nonzero(beta)
+        n = X.shape[0] # number of samples
 
-        if num_selected_features > 0:
-            ols_model = OLS(X, X_selected).fit()
-            bic_value = ols_model.bic
+        aic_value = 2*k  + n*np.log(RSS + eps / n)
 
-            # Check if this alpha value results in a lower BIC
-            if bic_value < best_bic:
-                best_bic = bic_value
-                best_alpha = alpha
-                best_model = curr_model
-
-            print(f"Trained with alpha = {alpha}. BIC = {bic_value}", flush=True)
-        else:
-            print(f"Trained with alpha = {alpha}. No features selected.", flush=True)
-
-        # Check if this alpha value results in a lower BIC
-        if bic_value < best_bic:
-            best_bic = bic_value
+        # Set the model with the lowest aic as the best model
+        if aic_value < best_aic:
+            best_aic = aic_value
             best_alpha = alpha
             best_model = curr_model
 
-        print(f"Trained with alpha = {alpha}.", flush=True)
+        # Calculate correlation metrics for log file and terminal output
+        pearson_correlation = r2_score(y, predy)
+        spearman_correlation = stats.spearmanr(y, predy).statistic
 
-    # Convert the model output to O matrix and return the best alpha and rule set.
-    if priors:
-        posterior_samples = best_model.get_samples()
-        best_solution = posterior_samples['beta'].mean(dim=0).numpy()
-    else:
-        best_solution = best_model.coef_
-    return best_alpha, best_solution
+        # Output to log file
+        if log_path:
+            with open(log_path, "a+") as log_file:
+                log_file.write(f"Trained with alpha = {alpha}. AIC = {aic_value}, pearson_c = {pearson_correlation}, spearman_c = {spearman_correlation}")
 
-def bayesian_lasso_regression(X: NDArray[np.float64], y: NDArray[np.float64], beta_true: NDArray[np.float64], alpha: int) -> tuple[NDArray[np.float64] | Any, NDArray[np.float64] | Any]:
-    """
-    Fit a Bayesian GLM using Pyro with a Lasso penalty.
+        # Output to terminal
+        print(f"Trained with alpha = {alpha}. AIC = {aic_value}, pearson_c = {pearson_correlation}, spearman_c = {spearman_correlation}", flush=True)
 
-    Parameters
-    ----------
-    X : array-like
-        Feature matrix (n_samples, n_features).
-    y : array-like
-        Response vector (n_samples,).
-    beta_true : array-like
-        Known true coefficients (n_features,).
-    alpha : float
-        The regularization strength for the Lasso penalty.
-
-    Returns
-    -------
-    posterior_samples : pyro trace object
-        Contains the posterior samples of the model parameters.
-    """
-    # Define the model in Pyro
-    def model(X, y):
-        # Define the Laplace prior for the coefficients (Lasso)
-        beta = pyro.sample('beta', dist.Laplace(torch.tensor(beta_true), torch.tensor(alpha)))
-        
-        # Likelihood: Linear model y = X * beta + noise
-        mu = torch.matmul(X, beta)
-        sigma = pyro.sample('sigma', dist.HalfNormal(torch.tensor(0.2)))  # Noise standard deviation (sigma)
-        
-        # Likelihood function for observed data (Normal distribution)
-        with pyro.plate('data', len(y)):
-            pyro.sample('obs', dist.Normal(mu, sigma), obs=y)
-    
-    # Using NUTS for MCMC sampling
-    nuts_kernel = NUTS(model)
-    mcmc = MCMC(nuts_kernel, num_samples=1000, warmup_steps=100)
-    mcmc.run(torch.tensor(X), torch.tensor(y))
-    
-    # Extract posterior samples for beta
-    posterior_samples = mcmc.get_samples()['beta']
-    
-    # Calculate the mean of the posterior samples for each coefficient
-    opt_beta = posterior_samples.mean(dim=0).numpy()
-
-    # Return the optimal beta and the model
-    return opt_beta, mcmc
+    return best_alpha, best_model.coef_
 
 class Subject:
     """
